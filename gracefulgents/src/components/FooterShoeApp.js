3d714611a4b7b5264d1296c2d1f30dc25021bc03
Nguyen3464 <n.nguyen3464@gmail.com>
2023-06-23 10:01:21 -0700

    shoeapp work

---
+++ gracefulgents/src/components/FooterShoeApp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FooterShoeApp = () => {
+  return (
+    <div className='footer-shoe-app'>
+        <h3>©️ Created by Nguyen Nguyen</h3>
+    </div>
+  )
+}
+
+export default FooterShoeApp
---
+++ gracefulgents/src/pages/ShoeAppIndex.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ShoeAppIndex = () => {
+  return (
+    <div>
+      <h1>test</h1>
+    </div>
+  )
+}
+
+export default ShoeAppIndex
